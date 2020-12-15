1511f0ccea5218db0f632f54e0b01707f35d5ba5
dfayika1988 <47186916+dfayika1988@users.noreply.github.com>
2020-12-15 14:52:57 +0000

    Update AngloAirflowCSV.py

--- Readingandwriting/AngloAirflowCSV.py
+++ Readingandwriting/AngloAirflowCSV.py
@@ -1,9 +1,9 @@
 import datetime as dt
-from datetime import timedelta
+from datetime import tdelta
 
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow import TIG
+from airflow.operators.bash_operator import BOperator
+from airflow.operators.python_operator import POperator
 
 import pandas as pd
 
@@ -16,7 +16,7 @@ def csvToJson():
 	
 
 
-default_args = {
+default_as = {
     'owner': 'Demilson',
     'start_date': dt.datetime(2020, 12, 15),
     'retries': 1,
@@ -24,16 +24,16 @@ default_args = {
 }
 
 
-with DAG('MyCSVDAG',
-         default_args=default_args,
-         schedule_interval=timedelta(minutes=5),      # '0 * * * *',
-         ) as dag:
+with TID('MyCSV',
+         default_args=default_as,
+         schedule_interval=tdelta(minutes=5),      # '0 * * * *',
+         ) as tid:
 
-    print_starting = BashOperator(task_id='starting',
+    print_starting = BOperator(task_id='starting',
                                bash_command='echo "I am reading the CSV now....."')
     
-    csvJson = PythonOperator(task_id='convertCSVtoJson',
-                                 python_callable=csvToJson)
+    csvJson = PyOperator(task_id='convertCSVtoJson',
+                                 python_ca=csvToJson)
 
 
 print_starting >> csvJson
