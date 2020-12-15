f8687605c35c2f05227ff76cfa239b501e6d4b02
dfayika1988 <47186916+dfayika1988@users.noreply.github.com>
2020-12-15 14:56:01 +0000

    Update Angloloadjson.py

--- Readingandwriting/Angloloadjson.py
+++ Readingandwriting/Angloloadjson.py
@@ -5,6 +5,6 @@ fake=Faker()
 alldata={}
 alldata['records']=[]
 for x in range(1000):
-	data={"name":fake.name(),"age":fake.random_int(min=18, max=80, step=1),"street":fake.street_address(),"city":fake.city(),"state":fake.state(),"zip":fake.zipcode(),"lng":float(fake.longitude()),"lat":float(fake.latitude())}
+	data={"name":fake.name(),"age":fake.random_int(min=18, max=80, step=1),"guid":fake.guid(),"birthday":fake.birthday()," signup_date":fake. signup_date(),"account_type":fake.account_type())}
 	alldata['records'].append(data)	
 json.dump(alldata,output)
