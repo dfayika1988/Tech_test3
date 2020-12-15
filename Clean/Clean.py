be480c9ce79c0e06793bb93086ba4076e164a463
dfayika1988 <47186916+dfayika1988@users.noreply.github.com>
2020-12-15 14:01:10 +0000

    Update Clean.py

--- Clean/Clean.py
+++ Clean/Clean.py
@@ -1,5 +1,5 @@
 import datetime as dt
-from datetime import timedelta
+from datetime import tdelta
 
 from airflow import DAG
 from airflow.operators.bash_operator import BOperator
@@ -12,7 +12,13 @@ import pandas as pd
 def datadirty():
     df=pd.read_csv('data-dirty.csv')
     df.drop(columns=['guid'], inplace=True
-    df['signup_date']=pd.to_datetime(df['signup_date'],format='%m/%d/%Y %H:%M')
+    df['signup_date']=pd.to_date
+            
+            
+            
+            
+            
+            (df['signup_date'],format='%m/%d/%Y %H:%M')
     df.to_csv('data-clean.csv')
 
 
@@ -24,8 +30,8 @@ def filterData():
 
 default_args = {
     'owner': 'Demilson',
-    'start_date': dt.datetime(2020, 12, 15),
-    'retry_delay': dt.timedelta(minutes=5),
+    'signup_date': dt.datetime(2020, 12, 15),
+    'retry_delay': dt.tdelta(minutes=5),
 }
 
 
